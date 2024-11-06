c4d89b5e72717d2549af43825551fb537e17823e
nrlizahh <nurhalizah.sn101@gmail.com>
2024-11-06 19:23:45 +0700

    seeding

--- models/post.js
+++ models/post.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Post.belongsTo(models.User, {foreignKey : 'UserId'})
+      Post.belongsToMany(Tag, { through: 'PostTag' });
     }
   }
   Post.init({
--- models/user.js
+++ models/user.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasOne(models.Profile, {foreignKey: 'UserId'})
+      User.hasMany(models.Post, {foreignKey: 'UserId'})
     }
   }
   User.init({
